a500b15002f6b0c8083c1b50d2a6d57792a126bf
EiPhyuSinn <eiphyusment@gmail.com>
2025-11-13 13:44:37 +0630

    Updated notebooks

--- setup.py
+++ setup.py
@@ -3,5 +3,12 @@ from setuptools import setup, find_packages
 setup(
     name="mlproject",
     packages=find_packages(),
-    install_requires=["numpy", "pandas"]
+    install_requires=["numpy", 
+                      "pandas", 
+                      "scikit-learn", 
+                      "seaborn", 
+                      "matplotlib",
+                      "catboost",
+                      "xgboost"
+                      ]
 )
